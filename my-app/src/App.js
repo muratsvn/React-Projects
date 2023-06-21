58c5f8bccdb044efb65704d4cf309a0752791282
User1 <sevenmuratt@gmail.com>
2023-06-21 15:21:17 +0300

    slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,18 @@ import ShopCard from "./components/01-shop-card/shop-card";
 import Main from "./components/02-home-tours/main";
 import Progress from "./components/03-progress-bar/progress";
 import Tabs from "./components/06-tabs/tabs";
+import Slider from "./components/07-slider/slider";
 function App() {
   return (
     <>
     {/* <ShopCard/>
     <Main/> 
-    <Progress/>*/}
+    <Progress/>
+    <Tabs/>
     
-    <Tabs/> 
+    */}
+    
+     <Slider/>
     </>
   ); 
 }
